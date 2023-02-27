1e83d023da88d60003ba65ebef858538b528b1dc
isayaksh <isayaksh@gmail.com>
2023-02-27 15:15:42 +0900

    Initial commit

---
+++ src/test/java/me/isayaksh/DatalabTrendShopping/DatalabTrendShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.isayaksh.DatalabTrendShopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatalabTrendShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
